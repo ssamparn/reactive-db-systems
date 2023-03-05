51de501f1856757630a2613cbe429162bdcd401c
samantrs <https://samantrs@git.rabobank.nl>
2023-03-05 14:09:10 +0100

    added r2dbc sample module

---
+++ r2dbc-postgresql-sample/src/main/java/com/reactive/database/r2dbcpostgresqlsample/R2dbcPostgresqlSampleApplication.java
@@ -0,0 +1,13 @@
+package com.reactive.database.r2dbcpostgresqlsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R2dbcPostgresqlSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R2dbcPostgresqlSampleApplication.class, args);
+	}
+
+}
---
+++ r2dbc-postgresql-sample/src/test/java/com/reactive/database/r2dbcpostgresqlsample/R2dbcPostgresqlSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactive.database.r2dbcpostgresqlsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R2dbcPostgresqlSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
